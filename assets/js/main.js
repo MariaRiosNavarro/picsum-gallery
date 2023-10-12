6fa8fdf743a5522477ef9bcf943f3585ee39b17e
SuperCoder <propscode254@gmail.com>
2023-10-12 19:23:54 +0200

    add modern async arrow version

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,75 +27,132 @@ next.addEventListener("click", () => {
   }
 });
 
-//# FOR the Pagination: Normally I would have
-//# the address (url) stored in a variable if I render only
-//# once, but for pagination we need it inside the
-//# function (to change the page number).
-//# So that when we call it in the
-//# event listener (prev/back)
-//# it works, outside it does not work (scope).
+// ! ---------Function with .then.then.catch Version comented---under that--MODERN ASYNC VERSION
 
-// let url = `https://picsum.photos/v2/list?page=${page}&limit=21`;
+// //# FOR the Pagination: Normally I would have
+// //# the address (url) stored in a variable if I render only
+// //# once, but for pagination we need it inside the
+// //# function (to change the page number).
+// //# So that when we call it in the
+// //# event listener (prev/back)
+// //# it works, outside it does not work (scope).
 
-// ! 5-------- fetch (Save fetch in one function,
-// !        with the parameter of the page that we change with the prev/next buttons
-// !        to call it in the prev und back eventListeners)
+// // let url = `https://picsum.photos/v2/list?page=${page}&limit=21`;
 
-function fetchAndRender(page) {
-  fetch(`https://picsum.photos/v2/list?page=${page}&limit=21`)
-    .then((response) => response.json())
-    .then((data) => {
-      // !  5A-----------IMPORTANT for Pagination:
-      // !    remove the old render/page to add the new page
-      // !    AND handle the visibility of the prev/next
-      // !    buttons in der first und last page
-      gallery.innerHTML = "";
+// // ! 5-------- fetch (Save fetch in one function,
+// // !        with the parameter of the page that we change with the prev/next buttons
+// // !        to call it in the prev und back eventListeners)
 
-      if (page === 1) {
-        prev.style.visibility = "hidden";
-      } else if (page === 47) {
-        next.style.visibility = "hidden";
-      } else {
-        prev.style.visibility = "visible";
-        next.style.visibility = "visible";
-      }
+// function fetchAndRender(page) {
+//   fetch(`https://picsum.photos/v2/list?page=${page}&limit=21`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       // !  5A-----------IMPORTANT for Pagination:
+//       // !    remove the old render/page to add the new page
+//       // !    AND handle the visibility of the prev/next
+//       // !    buttons in der first und last page
+//       gallery.innerHTML = "";
 
-      // !  5B-----------Create Gallery
-      const pictures = data;
-      pictures.forEach((pic) => {
-        // !  5C-----------Create the Card for each pic and add class (styles)
-        let pictureCard = document.createElement("article");
-        pictureCard.classList.add("picture-card");
-        let picture = document.createElement("img");
-        let artistName = document.createElement("h3");
-        let seeMoreButton = document.createElement("button");
+//       if (page === 1) {
+//         prev.style.visibility = "hidden";
+//       } else if (page === 47) {
+//         next.style.visibility = "hidden";
+//       } else {
+//         prev.style.visibility = "visible";
+//         next.style.visibility = "visible";
+//       }
 
-        // !  5D-----------Give the data to the created elements
-        artistName.textContent = pic.author;
-        picture.src = pic.download_url;
-        picture.width = pic.width;
-        picture.height = pic.height;
-        picture.alt = `${pic.author} Picture`;
-        picture.classList.add("images");
-        seeMoreButton.classList.add("button");
-        seeMoreButton.textContent = "See More";
+//       // !  5B-----------Create Gallery
+//       const pictures = data;
+//       pictures.forEach((pic) => {
+//         // !  5C-----------Create the Card for each pic and add class (styles)
+//         let pictureCard = document.createElement("article");
+//         pictureCard.classList.add("picture-card");
+//         let picture = document.createElement("img");
+//         let artistName = document.createElement("h3");
+//         let seeMoreButton = document.createElement("button");
 
-        // !  5E-----------Create the dom structure of the card,
-        // !  add all elements to PictureCard and this one to gallery:
-        pictureCard.append(picture, artistName, seeMoreButton);
-        gallery.append(pictureCard);
+//         // !  5D-----------Give the data to the created elements
+//         artistName.textContent = pic.author;
+//         picture.src = pic.download_url;
+//         picture.width = pic.width;
+//         picture.height = pic.height;
+//         picture.alt = `${pic.author} Picture`;
+//         picture.classList.add("images");
+//         seeMoreButton.classList.add("button");
+//         seeMoreButton.textContent = "See More";
 
-        // !  5F-----------EventListener for the seeMoreButton
-        seeMoreButton.addEventListener("click", () => {
-          window.open(pic.url, "_blank");
-        });
+//         // !  5E-----------Create the dom structure of the card,
+//         // !  add all elements to PictureCard and this one to gallery:
+//         pictureCard.append(picture, artistName, seeMoreButton);
+//         gallery.append(pictureCard);
+
+//         // !  5F-----------EventListener for the seeMoreButton
+//         seeMoreButton.addEventListener("click", () => {
+//           window.open(pic.url, "_blank");
+//         });
+//       });
+//     })
+//     .catch((error) => {
+//       console.error("Some Issue", error);
+//     });
+// }
+
+// // !  6----------- initialisation of the first page
+
+// fetchAndRender(page);
+
+// # ========= MODERN VERSION WITH ASYNC ARROW FUNCTION & Try/catch:
+
+const fetchAndRender = async (page) => {
+  try {
+    const response = await fetch(
+      `https://picsum.photos/v2/list?page=${page}&limit=21`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    gallery.innerHTML = "";
+
+    if (page === 1) {
+      prev.style.visibility = "hidden";
+    } else if (page === 47) {
+      next.style.visibility = "hidden";
+    } else {
+      prev.style.visibility = "visible";
+      next.style.visibility = "visible";
+    }
+
+    const pictures = data;
+    pictures.forEach((pic) => {
+      let pictureCard = document.createElement("article");
+      pictureCard.classList.add("picture-card");
+      let picture = document.createElement("img");
+      let artistName = document.createElement("h3");
+      let seeMoreButton = document.createElement("button");
+
+      artistName.textContent = pic.author;
+      picture.src = pic.download_url;
+      picture.width = pic.width;
+      picture.height = pic.height;
+      picture.alt = `${pic.author} Picture`;
+      picture.classList.add("images");
+      seeMoreButton.classList.add("button");
+      seeMoreButton.textContent = "See More";
+
+      pictureCard.append(picture, artistName, seeMoreButton);
+      gallery.append(pictureCard);
+
+      seeMoreButton.addEventListener("click", () => {
+        window.open(pic.url, "_blank");
       });
-    })
-    .catch((error) => {
-      console.error("Some Issue", error);
     });
-}
-
-// !  6----------- initialisation of the first page
+  } catch (error) {
+    console.error("Some Issue", error);
+  } finally {
+    // add cleanup code here (if needed it).
+  }
+};
 
 fetchAndRender(page);
